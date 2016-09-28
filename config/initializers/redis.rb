3d13c22093b78bcd1b3034db1fa1618b175e7c38
Dan Molloy <danieljmolloy1@gmail.com>
2016-09-28 22:18:32 +0100

    redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
