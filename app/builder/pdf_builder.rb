b20bc204001ee1f83b8d2347d7cb89a68b69bad4
Dan Molloy <danieljmolloy1@gmail.com>
2017-02-23 21:42:53 +0000

    pdf filler!

--- app/builder/pdf_builder.rb
+++ app/builder/pdf_builder.rb
@@ -2,8 +2,12 @@ class PDFBuilder < DocBuilder
 
   def build
     @template_dir = Rails.root.join("app", "templates", 'pdf')
-    filler_path = "#{@template_dir}/filler.html"
     html_doc_path = HTMLBuilder.new(doc: @doc).build(nozip: true)
+    @directory = "/tmp/#{@doc.filename}"
+    @story = @doc.story
+    @domain = check_domain
+    render_template('filler.html.erb', 'filler.html')
+    filler_path = "#{@directory}/filler.html"
     pdf = PDFKit.new(File.open("#{html_doc_path}"), margin_top: 10, margin_bottom: 10,
                                                     margin_left: 0, margin_right: 0, quiet: false,
                                                     header_html: filler_path, footer_html: filler_path)
