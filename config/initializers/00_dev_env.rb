b1e9d0371d832231834064abe0462aedb216c204
Daniel Molloy <danieljmolloy1@gmail.com>
2017-03-03 12:14:24 +0000

    Delete 00_dev_env.rb

--- config/initializers/00_dev_env.rb
+++
@@ -1,5 +0,0 @@
-unless Rails.env.production?
-  ENV['AWS_ACCESS_KEY_ID'] = "AKIAI4JSTTKLAN6UGKLQ"
-  ENV['AWS_SECRET_ACCESS_KEY'] = "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"
-  ENV['S3_BUCKET_NAME'] = "omnibuser"
-end
