a735b69daa4d5dd0585a02990592c94a50ee9c8b
Dan Molloy <danieljmolloy1@gmail.com>
2017-05-30 12:54:19 +0100

    use old css targets for QQ threadmarks

--- app/scraper/qq_scraper.rb
+++ app/scraper/qq_scraper.rb
@@ -2,13 +2,13 @@ class QQScraper < ForumScraper
   def get_story
     @css = {post: '#messageList .message',
             threadmark: '.message.hasThreadmark',
-            overlay_threadmark: '.overlayScroll.threadmarkList li',
+            overlay_threadmark: '.overlayScroll.threadmarks li',
             chapter_pub_date: '.messageInfo .messageMeta .datePermalink .DateTime',
             chapter_edit_date: '.messageInfo .editDate .DateTime',
             story_pub_date: '.message .primaryContent .messageMeta .datePermalink',
             avatar: '#messageList .message .avatar img',
-            threadmark_list_item: '.threadmarkListItem a',
-            chapter_threadmark_text: 'Threadmarks:',
+            threadmark_list_item: '.threadmarkItem a',
+            chapter_threadmark_text: 'Threadmark:',
             chapter_content: '.messageContent .messageText',
           }
 
