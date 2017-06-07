ca46adc6adca90b9762f68859805dc6fa1291f95
Dan Molloy <danieljmolloy1@gmail.com>
2017-06-07 18:33:06 +0100

    partial sv integration tests and fix for recent chapters css target

--- app/scraper/sv_scraper.rb
+++ app/scraper/sv_scraper.rb
@@ -2,7 +2,7 @@ class SVScraper < ForumScraper
   def get_story
     @css = {post: '#messageList .message',
             threadmark: '.message.hasThreadmark',
-            overlay_threadmark: '.overlayScroll.threadmarkList li',
+            overlay_threadmark: '.ThreadmarkIndex .threadmarkList li',
             chapter_pub_date: '.messageInfo .messageMeta .datePermalink .DateTime',
             chapter_edit_date: '.messageInfo .editDate .DateTime',
             story_pub_date: '.message .primaryContent .messageMeta .datePermalink',
--- spec/features/sv_spec.rb
+++ spec/features/sv_spec.rb
@@ -9,20 +9,24 @@ feature "Sufficient Velocity", js: true do
     visit root_path
   end
 
-  scenario "Scraping a single-chapter story" do
-    get_story single_chapter, extension: 'epub'
-    expect(page).to have_content('Download')
-  end
+  # scenario "Scraping a single-chapter story" do
+  #   get_story single_chapter, extension: 'epub'
+  #   expect(page).to have_content('Download')
+  # end
   scenario "Scraping all chapters from a multi-chapter story" do
-    get_story multi_chapter, extension: 'html'
+    get_story large_images, extension: 'html'
     expect(page).to have_content('Download')
   end
   scenario "Scraping one chapter from a multi-chapter story" do
-    get_story multi_chapter, extension: 'mobi', recent: true, count: 1
+    get_story large_images, extension: 'mobi', recent: true, count: 1
     expect(page).to have_content('Download')
   end
   scenario "Scraping 10 chapters from a story with fewer than 10 chapters" do
-    get_story multi_chapter, extension: 'pdf', recent: true, count: 10
+    get_story large_images, extension: 'pdf', recent: true, count: 10
+    expect(page).to have_content('Download')
+  end
+  scenario "Scraping a story with no threadmark for first post" do
+    get_story unthreadmarked_op
     expect(page).to have_content('Download')
   end
 end
